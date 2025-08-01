9df3552572625237626922ade1ccd695f9849a71
Durdana_Khalid <durdanakhalid98@gmail.com>
2025-08-01 17:19:00 +0500

    Update app.py

--- Flask_Web_App/app.py
+++ Flask_Web_App/app.py
@@ -25,5 +25,6 @@ def predict():
         return render_template('index.html', input_text=input_text, prediction=sentiment)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  
+    app.run(host="0.0.0.0", port=port) 
